aada4c983ad8a0bc560cd0c3b9979651c3787b1f
jmsantias <jmsantias99@gmail.com>
2023-10-30 21:05:04 -0300

    Práctica parcial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+   <div className='App'>
+    <h1>Carga de estudiantes</h1>
+    <form></form>
+    <Card />
+   </div>
+  );
+}
+
+export default App
